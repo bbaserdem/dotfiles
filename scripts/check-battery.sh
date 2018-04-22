1237ba7fef0f545b41fa536a7cf3cd7853328b88
Batuhan Baserdem <baserdem.batuhan@gmail.com>
2018-04-21 22:22:45 -0400

    Final stuff

--- scripts/check-battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-printf 'script started' | systemd-cat -t check-battery #write to log when script is called
-BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ; then
-        printf 'condition is true' | systemd-cat -t check-battery #write to log if condition is true
-        DISPLAY=:0 /usr/bin/notify-send -u low "battery" "$BATTINFO"
-fi
