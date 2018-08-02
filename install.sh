95dbe9baa14c475ead918f7f4e191260adf2669f
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-08-02 15:31:33 -0400

    Blaa

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ IF=$'\n\t'
 sudo usermod -a -G lock sbp
 
 # Place symlinks
-echo 'Symlinking...\n'
+echo 'Symlinking...'
 ln -s ~/.config/abcde.conf ~/.abcde.conf
 ln -s ~/.config/gtk2rc ~/.gtkrc-2.0
 ln -s ~/.config/tmux.conf ~/.tmux.conf
@@ -24,21 +24,30 @@ ln -s ~/.config/applications ~/.local/share/applications
 read -rsp $'Press any key to continue...\n' -n1 key
 
 # Neovim
-echo 'Setting up neovim...\n'
+echo 'Setting up neovim...'
 pip install --user neovim
 pip install --user neovim-remote
 pip2 install --user neovim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 nvim +PluginInstall +qall
 ~/.config/nvim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
-read -rsp $'Press any key to continue...\n' -n1 key
+read -rsp $'Press any key to continue...' -n1 key
 
 # ZIM
 echo 'Setting up ZIM...\n'
 git clone --recursive https://github.com/zimfw/zimfw ~/.config/zsh/zimfw
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.config/zsh/zimfw/modules/prompt/external-themes/powerlevel9k
 ln -s ~/.config/zsh/zimfw/modules/prompt/external-themes/powerlevel9k/powerlevel9k.zsh-theme ~/.config/zsh/zimfw/modules/prompt/functions/prompt_powerlevel9k_setup
-read -rsp $'Press any key to continue...\n' -n1 key
+read -rsp $'Press any key to continue...' -n1 key
+
+# Dropbox
+echo 'Preventing dropbox auto updates...'
+if [ -d ~/.dropbox-dist ]
+then
+    rm -rf ~/.dropbox-dist
+fi
+install --mode 0 --directory ~/.dropbox-dist
+read -rsp $'Press any key to continue...' -n1 key
 
 # Nemo
 gsettings set org.cinnamon.desktop.default-applications.terminal exec termite
