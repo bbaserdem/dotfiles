d95a4fc042bdbb2dd01a634b9076c93977c3655e
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-09-03 14:18:18 -0700

    Fixed exit stuff

--- i3blocks/blocklets/date.sh
+++ i3blocks/blocklets/date.sh
@@ -3,4 +3,43 @@
 . ${XDG_CONFIG_HOME}/i3blocks/colors.sh
 
 _col="${col_ind}"
-echo "<span color=${_col}></span> $(date '+%a %d, %b %Y')"
+
+get_lunar () {
+    _days=$(python -c 'from datetime import date as d;print((d.today()-d(1999,8,11)).days)')
+    _moon="$(echo "${_days} % 29.530588853" | bc | awk '{printf("%d",$1+.5)}')"
+    case "${_moon}" in
+        0|30)   _ico="" ;;
+        1)      _ico="" ;;
+        2)      _ico="" ;;
+        3)      _ico="" ;;
+        4)      _ico="" ;;
+        5)      _ico="" ;;
+        6)      _ico="" ;;
+        7)      _ico="" ;;
+        8)      _ico="" ;;
+        9)      _ico="" ;;
+        10)     _ico="" ;;
+        11)     _ico="" ;;
+        12)     _ico="" ;;
+        13)     _ico="" ;;
+        14)     _ico="" ;;
+        15)     _ico="" ;;
+        16)     _ico="" ;;
+        18)     _ico="" ;;
+        19)     _ico="" ;;
+        20)     _ico="" ;;
+        21)     _ico="" ;;
+        22)     _ico="" ;;
+        23)     _ico="" ;;
+        24)     _ico="" ;;
+        25)     _ico="" ;;
+        26)     _ico="" ;;
+        27)     _ico="" ;;
+        28)     _ico="" ;;
+        29)     _ico="" ;;
+        *)      _ico="" ;;
+    esac
+    echo "<span color=${_col}>${_ico}</span>"
+}
+
+echo "<span color=${_col}></span> $(date '+%a %d, %b %Y') $(get_lunar)"
--- i3blocks/blocklets/mpd.sh
+++ i3blocks/blocklets/mpd.sh
@@ -3,19 +3,30 @@
 . ${XDG_CONFIG_HOME}/i3blocks/colors.sh
 
 _col="${col_red}"
+_len=32
 
 # Pass the password in the block instance
 if [[ -n $BLOCK_INSTANCE ]]; then
-    password=("-h" "$BLOCK_INSTANCE@localhost")
+    _pass=("-h" "$BLOCK_INSTANCE@localhost")
 fi
 
-filter() {
-	sed 2q | tac | sed -e "s/\&/&amp;/g;/volume:/d;s/\[paused\].*/<span color=${_mute}>/g;s/\[playing\].*/<span>/g" | tr -d '\n' | sed -e "s/$/<\/span>/g" | sed -e "s/^/<span color=${_col}> <\/span>/g"
-	}
-
+# On click actions
 case $BLOCK_BUTTON in
-    1) mpc $password toggle | filter ;;  # left click, toggle
-    4) mpc $password prev   | filter ;;  # scroll up, previous
-    5) mpc $password next   | filter ;;  # scroll down, next
-    *) mpc $password status | filter ;;
+    1) mpc $_pass toggle > /dev/null ;;  # left click, toggle
+    3) $TERMINAL -e ncmpcpp & disown ;;     # Right click; lauch ncmpcpp
+    4) mpc $_pass prev > /dev/null ;;    # scroll up, previous
+    5) mpc $_pass next > /dev/null ;;    # scroll down, next
 esac
+
+# Prompt stuff
+_text="$(mpc $_pass status | head -n 1)"
+_icon="<span color=${_col}></span>"
+
+[ "${#_text}" -gt "${_len}" ] && _text="${_text:0:${_len}}…"
+
+if [ "$(mpc status | tail -n 2 | head -n 1 | awk '{ print $1 }')" == "[paused]" ]
+then
+    echo "${_icon} <span color=${_mute}>${_text}</span>"
+else
+    echo "${_icon} ${_text}"
+fi
