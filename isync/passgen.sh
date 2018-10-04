714b8e38594cbc47598f8778ef2edd5d6d5aab65
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-03 23:26:10 -0400

    Auto-sync: 2018-10-03_23:26:10

--- isync/passgen.sh
+++ isync/passgen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-_file="${XDG_CONFIG_HOME}/mbsync/password.sh"
+_file="${XDG_CONFIG_HOME}/mbsync/password-Gmail.sh"
 echo "#!/usr/bin/sh
 echo $(pass Google | grep 'app:' | awk '{print $2}')" > "${_file}"
 chmod 700 "${_file}"
