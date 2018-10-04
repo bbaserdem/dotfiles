ef7439a9858d67af6396862f35c3f3491da120c6
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-04 14:03:59 -0400

    Auto-sync: 2018-10-04_14:03:59

--- i3blocks/blocklets/language.sh
+++ i3blocks/blocklets/language.sh
@@ -7,7 +7,7 @@ _ico=""
 
 _sta="$(xkb-switch -p)"
 _lan="$(echo "${_sta}" | sed 's|\(.*\)(.*)|\1|' | awk '{print toupper($0)}')"
-_lay="$(echo "${_tmp}" | grep '(' | sed 's|.*(\(.*\))|\1|')"
+_lay="$(echo "${_sta}" | grep '(' | sed 's|.*(\(.*\))|\1|')"
 [ -z "${_lay}" ] && _lay="qwe" || _lay="${_lay:0:3}"
 
 echo "<span color=${_col}>${_ico}</span> ${_lan}(${_lay})" | sed 's|&|&amp;|g'
