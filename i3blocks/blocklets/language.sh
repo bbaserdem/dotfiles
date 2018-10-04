c1c03e78d92c5799126536049b5ecab249f0d787
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-04 13:55:30 -0400

    Auto-sync: 2018-10-04_13:55:30

---
+++ i3blocks/blocklets/language.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+. ${XDG_CONFIG_HOME}/i3blocks/colors.sh
+
+_col="${col_vio}"
+_ico=""
+
+_sta="$(xkb-switch -p)"
+_lan="$(echo "${_sta}" | sed 's|\(.*\)(.*)|\1|' | awk '{print toupper($0)}')"
+_lay="$(echo "${_tmp}" | grep '(' | sed 's|.*(\(.*\))|\1|')"
+[ -z "${_lay}" ] && _lay="qwe" || _lay="${_lay:0:3}"
+
+echo "<span color=${_col}>${_ico}</span> ${_lan}(${_lay})" | sed 's|&|&amp;|g'
