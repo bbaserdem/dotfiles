a6926deaf107f702438863a7cd1da91a3d452acb
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-04 22:20:08 -0400

    Auto-sync: 2018-10-04_22:20:08

--- scripts/hopper_rdp.sh
+++ scripts/hopper_rdp.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/sh
 
-_ydf=62
+_ydf=20
 _xdf=0
 
+if pgrep -x "i3" > /dev/null
+then
+    _dim="$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).rect | "\(.width)x\(.height)"')"
+    echo "Getting i3 geometry... ${_dim}"
+elif pgrep -x "sway" > /dev/null
+then
+    _dim="$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused==true).rect | "\(.width)x\(.height)"')"
+    echo "Getting sway geometry... ${_dim}"
+elif pgrep -x "Xorg" > /dev/null
+then
+    _dim="$(xrandr | grep \* | awk '{print $1}' | head -n 1)"
+    echo "Using xrandr... ${_dim}"
+else
+    _dim="1920x1080"
+    echo "Using default... ${_dim}"
+fi
+
 # Get screen dimensions
-_dim="$(xrandr | grep \* | awk '{print $1}' | head -n 1)"
 _xdm="$(echo "${_dim}" | sed 's/\([0-9]*\)x[0-9]*/\1/')"
 _ydm="$(echo "${_dim}" | sed 's/[0-9]*x\([0-9]*\)/\1/')"
 _res="$(( $_xdm - $_xdf ))x$(( $_ydm - $_ydf ))"
@@ -16,4 +32,7 @@ _srv="$(echo "${_inf}" | grep 'domain:'   | awk '{print $2}')"
 _prt="$(echo "${_inf}" | grep 'port:'     | awk '{print $2}')"
 _adr="${_srv}"':'"${_prt}"
 
-xfreerdp /u:"${_una}" /p:"${_psw}" /size:"${_res}" -grab-keyboard /v:"${_srv}${_prt}"
+_cmd="xfreerdp /u:${_una} /p:${_psw} /size:${_res} /wm-class:'Hopper-RDP' /title:'' -grab-keyboard -wallpaper -themes /v:${_srv}"
+
+echo "${_cmd}"
+"${_cmd}"
