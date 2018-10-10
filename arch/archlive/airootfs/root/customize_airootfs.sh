0a795de7623fa2734ef10bcf105aa49c5d8ffa78
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-09 23:10:25 -0400

    Auto-sync: 2018-10-09_23:10:25

--- arch/archlive/airootfs/root/customize_airootfs.sh
+++ arch/archlive/airootfs/root/customize_airootfs.sh
@@ -23,7 +23,7 @@ systemctl set-default graphical.target
 useradd --create-home --groups wheel --shell /usr/bin/zsh arch
 
 # Change default session
-sed -i 's|^#user-session=.*|user-session=xfce|g' /etc/lightdm/lightdm.conf
+sed -i 's|^#user-session=.*|user-session=XFCE|g' /etc/lightdm/lightdm.conf
 
 # Do passwords
 echo "root:iusearchbtw" | chpasswd
