11263aa4c4bc47a27e0379273156afc2442a5ca1
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2018-10-11 21:50:20 -0400

    Auto-sync: 2018-10-11_21:50:20

--- s3cfg-gen.sh
+++ s3cfg-gen.sh
@@ -2,4 +2,4 @@
 
 echo "[default]
 access_key = $(pass AWS|grep 'access-key-id'   |awk '{print $2'})
-secret_key = $(pass AWS|grep 'secret-access-id'|awk '{print $2'})" > ~/.s3cfg
+secret_key = $(pass AWS|grep 'secret-access-key'|awk '{print $2'})" > ~/.s3cfg
