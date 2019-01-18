92aa2f503506d2ab73463664da206d8877934a1b
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2019-01-18 16:39:09 -0500

    Auto-sync: 2019-01-18_16:39:09

--- install.sh
+++ install.sh
@@ -23,8 +23,6 @@ ln -sf ~/.config/X11/clientrc ~/.xinitrc
 ln -sf ~/.config/X11/serverrc ~/.xserverrc
 ln -sf ~/.config/X11/resources ~/.Xresources
 ln -sf ~/.config/X11/profile ~/.xprofile
-ln -sf ~/.config/X11/keymap ~/.Xkbmap
-ln -sf ~/.config/X11/modmap ~/.Xmodmap
 ln -sf ~/.config/X11/session ~/.xsession
 ln -sf ~/.config/matlabrc.m ~/Documents/MATLAB/startup.m
 rm -rf ~/.local/share/applications
