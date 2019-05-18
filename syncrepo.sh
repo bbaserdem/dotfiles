f6fec8d6068518938b1ad28f1a1baef6fae7f14b
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2019-05-17 21:41:09 -0400

    Autosync by sbpnotebook at 2019-05-17_21:41:09

--- syncrepo.sh
+++ syncrepo.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-_repo=${1:$XDG_CONFIG_HOME}
+_repo=${1:-$XDG_CONFIG_HOME}
 
 # Sync the config repo
 /usr/bin/git -C "${_repo}" pull
 
 # Check if there are conflicts
-if [ "$(git -C ${_repo} ls-files -u | wc -l)" -gt 0 ] ; then
+if [ "$(/usr/bin/git -C "${_repo}" ls-files -u | wc -l)" -gt 0 ] ; then
     _txt="Repo at ${_repo} has conflicting files, aborting"
     /usr/bin/notify-send 'Git Conflict' "${_txt}"
     exit 1
 fi
 
 # Push changes
-if [ -z "$(/usr/bin/git status -C ${_repo} status --porcelain)" ] ; then
+if [ ! -z "$(/usr/bin/git -C "${_repo}" status --porcelain)" ] ; then
+    echo "Changes detected"
     _message="Autosync by $(/bin/hostname) at $(date +%Y-%m-%d_%H:%M:%S)"
     /usr/bin/git -C "$_repo" add -A
     /usr/bin/git -C "$_repo" commit -m "${_message}"
