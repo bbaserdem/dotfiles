2983d2f6f775ca641315801462a8df99c195d2d9
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-04-06 12:00:01 -0400

    Autosync by sbp-homestation at 2020-04-06_12:00:01

--- bspwm/external_rules.sh
+++ bspwm/external_rules.sh
@@ -32,7 +32,7 @@ case $class in
     Steam|Stepmania)
         DESKTOP="${ws4}";;
     # Desktop 5: Other desktops
-    Qemu*|Virt-manager|*Remmina|transmission*|zoom|Skype)
+    Qemu*|Virt-manager|*Remmina|transmission*|Skype)
         DESKTOP="${ws5}";;
     # Desktop 6: Terminals
     Alacritty|kitty)
@@ -47,7 +47,7 @@ case $class in
     pdfsam|Blender|openscad|Picard*|Audacity|TuxGuitar)
         DESKTOP="${ws9}";;
     # Desktop 10: Settings
-    Pavucontrol|Syncthing*|dropbox|System*|Blueman*|Picard*|Maestral*|Pamac*|Ibus*|Firewall*)
+    Pavucontrol|Syncthing*|dropbox|System*|Blueman*|Picard*|Maestral*|Pamac*|Ibus*|Firewall*|zoom)
         DESKTOP="${ws0}";;
 esac
 
@@ -80,6 +80,10 @@ case $title in
     # Save prompts do not go to a new desktop
     Export*|Save*|Open*|Quit*|File*|Insert*|Confirm*|Playlist*|TabCompletionPopup)
         DESKTOP=""
+        ;;&
+    # Zoom meeting go to remote
+    'Zoom Meeting ID'*|'zoom_linux_float_video_window')
+        DESKTOP="${ws5}"
         ;;&
 esac
 
