4f07b1ca289bf2d241fb8ac328d486d3d4df2de4
Batuhan Baserdem <baserdem.batuhan@gmail.com>
2020-04-18 16:00:00 -0400

    Autosync by sbp-workstation at 2020-04-18_16:00:00

--- bspwm/polylaunch.sh
+++ bspwm/polylaunch.sh
@@ -15,13 +15,13 @@ fi
 
 # Run this code block
 ( flock 200
-    # Iterate over all running polybar instances
-    #---Kill all running polybars in this monitor
+    #---Kill all running polybars on this display
     pgrep 'polybar' | while IFS= read -r _pid ; do
         # Check the DISPLAY variable of this instance is the same with this one
         #   and terminate that bar if it is
-        _dvar="$( ( tr '\0' '\n' | awk -F '=' '/DISPLAY/ {print $2}' ) \
-            < "/proc/$1/environ")"
+        _dvar="$( \
+          ( tr '\0' '\n' | awk -F '=' '/DISPLAY/ {print $2}' ) \
+          < "/proc/${_pid}/environ")"
         if [ "${_dvar}" = "${DISPLAY}" ]; then
             kill "${_pid}"
         fi
@@ -31,33 +31,36 @@ fi
         done
     done
 
-    # Get outputs from bspwm, and launch polybar on each monitor
+    #---Launch polybar on all monitors
     polybar --list-monitors | while IFS= read -r _pom ; do
-        # Get the horizontal resolution number of the monitor
-        _mon="$(awk '{print $1}' <<< "${_pom}")"
-        _hor="$(sed 's|.* \([0-9]\+\)x[0-9]\+.*|\1|' <<< "${_pom}")"
+
+        #--Format log location
+        _mon="$(awk --field-seperator='[ ,:]' '{print $1}' <<< "${_pom}")"
         _log="${XDG_DATA_HOME}/polybar/log-${DISPLAY}-${_mon}"
         if [ ! -d "$(dirname "${_log}")" ] ; then
             mkdir -p "$(dirname "${_log}")"
         fi
-        # Check if high-dpi need to be used
+        export POLYMON="${_mon}"
+
+        #--Get the horizontal resolution to differentiate hidpi
+        _hor="$(sed 's|.* \([0-9]\+\)x[0-9]\+.*|\1|' <<< "${_pom}")"
         if [ "${_hor}" -gt '2000' ] ; then
-            if grep -q 'primary' <<< "${_pom}" ; then
-                polybar --reload top-hi </dev/null >"${_log}-top" 2>&1 & disown
-                polybar --reload bot-hi </dev/null >"${_log}-bot" 2>&1 & disown
-            else
-                POLYMON="${_mon}" polybar --reload aux-hi </dev/null >"${_log}-aux" 2>&1 & disown
-            fi
+            _suf='-hi'
         else
-            if grep -q 'primary' <<< "${_pom}" ; then
-                polybar --reload top </dev/null >"${_log}-top" 2>&1 & disown
-                polybar --reload bot </dev/null >"${_log}-bot" 2>&1 & disown
-            else
-                POLYMON="${_mon}" polybar --reload aux </dev/null >"${_log}-aux" 2>&1 & disown
-            fi
+            _suf=''
+        fi
+
+        # Launch the bars
+        if grep -q 'primary' <<< "${_pom}" ; then
+            # Launch the main bars
+            polybar --reload "top${_suf}" </dev/null >"${_log}-t" 2>&1 & disown
+            polybar --reload "bot${_suf}" </dev/null >"${_log}-b" 2>&1 & disown
+        else
+            # Launch an auxillary bar
+            polybar --reload "aux${_suf}" </dev/null >"${_log}-a" 2>&1 & disown
         fi
     done
-    # Close the file descriptor; ending the lock
-    exec 200<&-
 
+    # Close the file lock placed
+    flock --unlock 200
 ) 200>"${lock}"
