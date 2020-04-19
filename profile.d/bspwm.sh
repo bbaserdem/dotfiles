b4e4136905f0e6655f97d1dd35f2b5070a0ef8a8
Batuhan Baserdem <baserdem.batuhan@gmail.com>
2020-04-18 21:49:13 -0400

    Auto-sync: 2020-04-18_21:49:13

--- profile.d/bspwm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# BSPWM variable names
-export SBP_XPAPER_DIR="${HOME}/Pictures/Wallpapers"
--- profile.d/x11.sh
+++ profile.d/x11.sh
@@ -1,3 +1,5 @@
 # Location of X11 config files
 export XINITRC="${XDG_CONFIG_HOME}/X11/clientrc"
 export XSERVERRC="${XDG_CONFIG_HOME}/X11/serverrc"
+# User variable names
+export SBP_XPAPER_DIR="${HOME}/Pictures/Wallpapers"
