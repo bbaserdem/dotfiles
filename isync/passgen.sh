f0f38895288015bd4887236e931cc85b116979ba
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-04-27 18:29:55 -0400

    Reenabled mail synching

--- isync/passgen.sh
+++ isync/passgen.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-
-_file="${HOME}/.cache/isync/password-Gmail.sh"
-if [ -z "${_file}" ] ; then rm "${_file}" ; fi
+# The output script file
+_file="${XDG_DATA_HOME}/isync/password-Gmail.sh"
+# Create directory if not there
+if [ ! -z "$(dirname "${_file}")" ] ; then
+  mkdir -p "$(dirname "${_file}")"
+fi
+# If the script exists, delete it
+if [ -z "${_file}" ] ; then
+  rm "${_file}"
+fi
+# Echo appropriate lines to the script
 echo "#!/bin/sh" > "${_file}"
-echo "$(pass Google | grep 'app:' | awk '{print $2}')" > "${_file}"
+echo "$(pass Google | awk '/app:/ {print "echo", $2}')" >> "${_file}"
+# Make executable
 chmod 700 "${_file}"
