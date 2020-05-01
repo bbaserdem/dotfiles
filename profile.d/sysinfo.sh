e30f3d8d368ec28e3e94b0312f6818645d93ef58
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-05-01 00:07:32 -0400

    Changed a few things

--- profile.d/sysinfo.sh
+++ profile.d/sysinfo.sh
@@ -4,6 +4,7 @@ export SYSINFO_CAL_LOC="${HOME}/Documents/Calendar"
 export SYSINFO_CAL_MAXLEN=15
 export SYSINFO_CPU_POLL=5
 export SYSINFO_DATE_STYLE='reg'
+export SYSINFO_DISK_POLL=900
 export SYSINFO_EMAIL_LOC="${HOME}/Documents/Mail/Gmail"
 export SYSINFO_MEM_POLL=30
 export SYSINFO_MPD_POLL=30
