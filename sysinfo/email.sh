ea4cac3e3e97feec2c543a97fa98fe493d0eb4a9
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-05-05 20:35:30 -0400

    Cleaned email setup

--- sysinfo/email.sh
+++ sysinfo/email.sh
@@ -11,10 +11,10 @@ trap 'kill 0' EXIT
 # | -_|     | .'| | | #
 # |___|_|_|_|__,|_|_| #
 #######################
-# Email module (depends on inotifp)
+# Email module (depends on inotify)
 #  * Can specify location using SYSINFO_EMAIL_LOC
 if [ -z "${SYSINFO_EMAIL_LOC}" ] ; then
-  SYSINFO_EMAIL_LOC="${HOME}/Documents/Mail/Gmail"
+  SYSINFO_EMAIL_LOC="${HOME}/Mail"
 fi
 click_right  () {
   ( flock --nonblock 7 || exit 7
@@ -26,19 +26,29 @@ click_right  () {
   ) 7>"${SYSINFO_FLOCK_DIR}/${name}_${IDENTIFIER}_right" >/dev/null 2>&1 &
 }
 print_info () {
+  # Get inbox location and prefix info
+  if [ -z "${instance}" ] ; then
+    instance='gmail'
+  fi
+  maildir_location="${SYSINFO_EMAIL_LOC}/${instance}"
+  # Set the prefix; depending on provider
+  case "${instance}" in
+    gmail) pre=' ' ;;
+    *)     pre=' '
+  esac
   # If there is no inbox; just exit
-  if [ ! -d "${SYSINFO_EMAIL_LOC}/Inbox/new" ] ; then
+  if [ ! -d "${maildir_location}/inbox/new" ] ; then
     empty_output
     exit 1
   fi
   # Get number of new emails
-  txt="$(find "${SYSINFO_EMAIL_LOC}/Inbox/new" | wc -l)"
-  # Set up prefix
+  txt="$(find "${maildir_location}/inbox/new" | wc -l)"
+  # Set up suffix
   if [ "${txt}" -le 0 ] ; then
-    pre=" "
+    suf=" "
     feature='mute'
   else
-    pre=" "
+    suf=" "
   fi
   # Print string
   formatted_output
@@ -50,7 +60,7 @@ print_loop () {
   while : ; do
     inotifywait --recursive --timeout -1 \
       --event modify --event move --event create --event delete \
-      "${SYSINFO_EMAIL_LOC}" >/dev/null 2>&1 || exit 2
+      "${maildir_location}" >/dev/null 2>&1 || exit 2
     print_info || exit
     # Wait a while before trying to add watches again
     sleep 1
