67a1437ea3ec90fa58fc4eefa211fabc66e23886
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-05-12 22:04:55 -0400

    Added uptime, also put disk modules in polybar

---
+++ sysinfo/uptime.sh
@@ -0,0 +1,37 @@
+#!/bin/dash
+# vim:ft=sh
+
+# Kill all descendents on exit
+trap 'exit' INT TERM
+trap 'kill 0' EXIT
+
+#############################
+#          _   _            #
+#  _ _ ___| |_|_|_____ ___  #
+# | | | . |  _| |     | -_| #
+# |___|  _|_| |_|_|_|_|___| #
+#     |_|                   #
+#############################
+# Uptime module
+#  * Does not take arguments; but polling can be changed
+if [ -z "${SYSINFO_UPTIME_POLL}" ] ; then
+  SYSINFO_UPTIME_POLL=60
+fi
+print_info () {
+  pre=' '
+  time="$(uptime)"
+  # Get the hours:minutes; which is <1 or 2 numbers>:<2 numbers>
+  hrs="$(echo "${time}" | sed -n 's|.*\([0-9]\+:[0-9][0-9]\),.*|\1|p')"
+  day="$(echo "${time}" | sed -n 's|.*\([0-9]\+ days\?\).*|\1, |p')"
+  sol="$(echo "${time}" | sed -n 's|.*\([0-9]\+ years\?\).*|\1, |p')"
+  txt="${sol}${day}${hrs}"
+  # Print string
+  formatted_output
+}
+
+print_loop () {
+  while : ; do
+    print_info
+    sleep "${SYSINFO_UPTIME_POLL}"
+  done
+}
