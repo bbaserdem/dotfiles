452a7f31b9fc87d80c506a0cb0c6534bfb5cc2f2
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2020-10-02 14:56:56 -0400

    Fixed breaking change

--- mpdscribble-confgen.sh
+++ mpdscribble-confgen.sh
@@ -11,22 +11,20 @@ if [ -z "${_file}" ] ; then
 fi
 
 # Create config base
-echo "[mpdscribble]
-verbose = 2
+echo "
 host = localhost
 port = 6600
-log = ${HOME}/.cache/mpdscribble/mpdscribble.log
+log = ${XDG_CACHE_HOME}/mpdscribble/mpdscribble.log
 verbose = 2
 proxy = none
 
 [last.fm]
 url = http://post.audioscrobbler.com/
-journal =  ${HOME}/.cache/mpdscribble/lastfm.journal" > "${_file}"
+journal =  ${XDG_CACHE_HOME}/mpdscribble/lastfm.journal" > "${_file}"
 
 # Do login info
 echo "username = $(pass LastFM | awk '/username:/ {print $2}')" >> "${_file}"
 echo "password = $(pass LastFM | head -n 1)" >> "${_file}"
-#echo "password = $(pass LastFM|head -n 1|md5sum|awk '{print $1}')" >> "${_file}"
 
 # Fix permission
 chmod 700 "${_file}"
