5695f28906f135a4896965adee662462634bff50
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2022-07-19 11:44:24 -0400

    Fixed nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -280,14 +280,12 @@ M.this_setup = require('packer').startup(function(use)
         config = function()
             require('mkdnflow').setup({
                 create_dirs = true,
-                links_relative_to = 'current',
                 filetypes = {
                     md = true,
                     rmd = true,
                     markdown = true,
                 },
-                wrap_to_beginning = true,
-                wrap_to_end = true,
+                wrap = true,
             })
         end,
     }-- File type highlighting
