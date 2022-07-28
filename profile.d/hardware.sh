8fd27df389db944be7fc7aabb1375439c6fd9cf4
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2022-07-27 22:31:37 -0400

    Things

--- profile.d/hardware.sh
+++ profile.d/hardware.sh
@@ -1,8 +1,5 @@
 # Monitors
-if [ "$(hostname)" = 'sbp-homestation' ] ; then
-    export BRI_SCR="amdgpu_bl0"
-    export BRI_KBD="asus::kbd_backlight"
-elif [ "$(hostname)" = 'sbp-laptop' ] ; then
+if [ "$(uname -n)" = 'sbp-arch-laptop' ] ; then
     export BRI_SCR="intel_backlight"
     export BRI_KBD="system76_acpi::kbd_backlight"
 fi
